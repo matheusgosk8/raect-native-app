12c6c3bbf1fc756d9ab5344bdcf608c0ca2dc1a3
Matheus Lima <mtsgosk8@hotmail.com>
2025-01-25 03:27:29 -0300

    jddj

--- modern-app-server/migrations/0001_slim_franklin_storm.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "medicos" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"nome" text NOT NULL,
-	"especialidade" text NOT NULL,
-	"horarios_disponiveis" text
-);
---> statement-breakpoint
-DROP TABLE "users" CASCADE;
